94555bab6ab964f194574261ac86f1178fa3e9e1
ch-tstrickland <tstrickland@companieshouse.gov.uk>
2019-11-19 16:08:43 +0000

    Comment

--- src/test/java/parser/MailParserTest.java
+++ src/test/java/parser/MailParserTest.java
@@ -22,11 +22,19 @@ class MailParserTest {
         MailParser mailParser = new MailParser(msgIs);
         byte[] extractedCsvAttachment = mailParser.extractCsvAttachment();
         String extractedCsvAsString = new String(extractedCsvAttachment);
+        // Note that an exact match could not be obtained, byte-for-byte,
+        // so the looser contains is used in these tests.
+        // The actual decoded bytes seem to start with 0xFF, not a legal UTF-8 character.
+        // These are real emails being decoded (albeit anonymised) with real attachments.
+        // I can only assume that the start bytes are something weird like magic file bytes.
+        // The resulting string does decode as CSV, so we have chosen not to worry.
+        // The same applies for every positive test, using contains rather than an exact match.
         assertTrue(extractedCsvAsString.contains(EXPECTED));
     }
 
     @Test
-    void mailContainingMultipleCsvIsDecodableAndFirstCsvFileIsUsedOthersIgnored() throws MessagingException, IOException {
+    void mailContainingMultipleCsvIsDecodableAndFirstCsvFileIsUsedOthersIgnored()
+                    throws MessagingException, IOException {
         InputStream msgIs = getFileInputStream("src/test/resources/multiCsv.eml");
         MailParser mailParser = new MailParser(msgIs);
         byte[] extractedCsvAttachment = mailParser.extractCsvAttachment();
